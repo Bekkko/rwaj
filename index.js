a8ccc3ba82cf20c7641a499401d9340c7c3ce9ab
Bekkko <103800569+Bekkko@users.noreply.github.com>
2022-04-16 06:23:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@component/Foter'
+import Footer from '@components/Foter'
 
 export default function Home() {
   return (
