bfbfb8818d8a37ea52be28f7e6bbf036bfd3b0af
Bekkko <103800569+Bekkko@users.noreply.github.com>
2022-04-16 06:04:07 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Footer from '@components/Foter'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>rwa2!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
