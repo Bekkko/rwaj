57b94b89b50d870eb723a7b573610b1d9af23050
Bekkko <103800569+Bekkko@users.noreply.github.com>
2022-04-16 06:32:07 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+    <div className="container">
+
+      <Head>
+
+        <title>rwa2!</title>
+
+        <link rel="icon" href="/favicon.ico" />
+
+      </Head>
+
+  <main>
+        <Header title="نكتب من اجل تعلم المهارات الرائجه في روائج" />
+        <p className="description">
+          انكتب من اجلالاتصال  نستعرض اهكم تفسير الاحلام والاهتمام والعنايه بالشعر والجسم واهم اطلالات الموضه 
+    اساليب الحياه المختلفه وكل ماهو رائج والمهمات
+<a href="rwa2ej.com">↖️</a> <a href="https://rwa2ej.com/" target="_blank">روائج</a>
